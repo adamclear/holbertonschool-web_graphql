f2623d4537d7aae3a54cdb6568cf0b1eca160804
adamclear <3589@holbertonschool.com>
2022-11-11 19:46:03 +0000

    files for task 2

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const lodash = require('lodash');
 
 const TaskType = new graphql.GraphQLObjectType({
 	name: 'Task',
@@ -17,10 +18,30 @@ const RootQuery = new graphql.GraphQLObjectType({
 			type: TaskType,
 			args: {
 				id: {type: graphql.GraphQLID}
-			}},
-		resolve: (parent, args) => {}
+			},
+		resolve: (parent, args) => {
+			return lodash.find(tasks, { id: args.id });
+		}
+		}
 	})
 });
+
+const tasks = [
+	{
+    id: '1',
+    projectId: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+  {
+    id: '2',
+    projectId: '1',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+];
 
 const schema = new graphql.GraphQLSchema({
 	query: RootQuery,
