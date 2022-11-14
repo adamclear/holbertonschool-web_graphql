a5387220355f44773297bac2c776dc1c11b7e403
adamclear <3589@holbertonschool.com>
2022-11-14 18:49:45 +0000

    files for task 9

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -13,7 +13,7 @@ const TaskType = new graphql.GraphQLObjectType({
 		project: {
 			type: TaskType,
 			resolve: (parent, args) => {
-				return lodash.find(projects, {id: parent.projectId});
+				return Project.findById(parent.projectId);
 			}
 		}
 	})
@@ -29,7 +29,7 @@ const ProjectType = new graphql.GraphQLObjectType({
 		tasks: {
 			type: new graphql.GraphQLList(TaskType),
 			resolve: (parent, args) => {
-				return lodash.filter(tasks, {projectId: parent.id});
+				return Task.find({projectId: parent.id});
 			}
 		}
 	})
@@ -44,7 +44,7 @@ const RootQuery = new graphql.GraphQLObjectType({
 				id: {type: graphql.GraphQLID}
 			},
 			resolve: (parent, args) => {
-				return lodash.find(tasks, {id: args.id});
+				return Task.findById(args.id);
 			}
 		},
 		project: {
@@ -53,19 +53,19 @@ const RootQuery = new graphql.GraphQLObjectType({
 				id: {type: graphql.GraphQLID}
 			},
 			resolve: (parent, args) => {
-				return lodash.find(projects, {id: args.id});
+				return Project.findById(args.id);
 			}
 		},
 		tasks: {
 			type: new graphql.GraphQLList(TaskType),
 			resolve: () => {
-				return tasks;
+				return Task.find({});
 			}
 		},
 		projects: {
 			type: new graphql.GraphQLList(ProjectType),
 			resolve: () => {
-				return projects;
+				return Project.find({});
 			}
 		}
 	}),
@@ -110,37 +110,39 @@ const Mutation = new graphql.GraphQLObjectType({
 	})
 });
 
-const tasks = [
-	{
-    id: '1',
-    projectId: '1',
-    title: 'Create your first webpage',
-    weight: 1,
-    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-  },
-  {
-    id: '2',
-    projectId: '1',
-    title: 'Structure your webpage',
-    weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-  },
-];
+// Commented out the arrays for previous tasks
 
-const projects = [
-  {
-    id: '1',
-    title: 'Advanced HTML',
-    weight: 1,
-    description: `Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” its normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!`
-  },
-  {
-    id: '2',
-    title: 'Bootstrap',
-    weight: 1,
-    description: `Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.`
-  },
-];
+// const tasks = [
+// 	{
+//     id: '1',
+//     projectId: '1',
+//     title: 'Create your first webpage',
+//     weight: 1,
+//     description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+//   },
+//   {
+//     id: '2',
+//     projectId: '1',
+//     title: 'Structure your webpage',
+//     weight: 1,
+//     description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+//   },
+// ];
+
+// const projects = [
+//   {
+//     id: '1',
+//     title: 'Advanced HTML',
+//     weight: 1,
+//     description: `Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” its normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!`
+//   },
+//   {
+//     id: '2',
+//     title: 'Bootstrap',
+//     weight: 1,
+//     description: `Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.`
+//   },
+// ];
 
 const schema = new graphql.GraphQLSchema({
 	query: RootQuery,
